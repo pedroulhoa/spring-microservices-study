137d9c877b4f6baceaf5e37fbe819946991d62ed
Pedro Ulhoa <pedroulhoa@live.com>
2021-06-07 20:57:27 -0300

    hr-user data.sql / bcrypt test

---
+++ hr-user/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO user (name, email, password) VALUES ('Pedro', 'pedro@mail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO user (name, email, password) VALUES ('Jon', 'jon@mail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+
+INSERT INTO role (role_name) VALUES ('ROLE_OPERATOR');
+INSERT INTO role (role_name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
