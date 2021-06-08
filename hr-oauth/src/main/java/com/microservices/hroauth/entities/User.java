1411220db80ec9cd7743707a2b1651f923b559f1
Pedro Ulhoa <pedroulhoa@live.com>
2021-06-08 15:16:55 -0300

    hr-oauth configs jwt, tokens, etc

--- hr-oauth/src/main/java/com/microservices/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/microservices/hroauth/entities/User.java
@@ -1,10 +1,16 @@
 package com.microservices.hroauth.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User {
+public class User implements UserDetails {
 
     private Long id;
     private String name;
@@ -46,12 +52,44 @@ public class User {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public Set<Role> getRoles() {
